62abfed0a1e9419612f7528f24b6d228b1332642
awatagiltoo1 <awagitagiltoo@gmail.com>
2024-12-06 11:25:51 +0000

    mise à jour jira reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,7 +27,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'html',
-  reporter: [["line"], ["allure-playwright"],["jira-reporter"]],
+  reporter: [["line"], ["allure-playwright"],['./jira-reporter.ts']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
