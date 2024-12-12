6fa5454d2a18d1c1cb96317dc96edcdfed225a5a
ousseynouagiltoofr <elhadjiousseynou.diop95@gmail.com>
2024-12-12 10:20:56 +0000

    modification configuration

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,7 +4,7 @@ test('@MA-15 connexion', async({ page})=>{
 
   await page.goto('https://app-dev.mossall.com/auth/login');
   await page.getByLabel('Email').fill('genieouzog+atos@gmail.com');
-  await page.getByLabel('Mot de passe').fill('$Mossal1234');
+  await page.getByLabel('Mot de passe').fill('$Mossall123');
   await page.getByRole('button', { name: 'Se connecter' }).click();
 
 });
